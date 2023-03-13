555ce7cabacc78e7449ad4ebcb52c0dd0b41c231
Luc <67204364+YEET200@users.noreply.github.com>
2023-03-13 09:25:39 -0500

    Update app.cs

--- app.cs
+++ app.cs
@@ -1,1 +1,44 @@
+public class AppInfo
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
+}
+
+public class AppInfo
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
+}
+
+public MainWindow()
+{
+    InitializeComponent();
+
+    List<AppInfo> apps = new List<AppInfo>();
+
+    string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
+    foreach (string dir in Directory.GetDirectories(programFiles))
+    {
+        string appName = new DirectoryInfo(dir).Name;
+        string appExePath = Path.Combine(dir, appName + ".exe");
+
+        if (File.Exists(appExePath))
+        {
+            apps.Add(new AppInfo { Name = appName, Path = appExePath });
+        }
+    }
+
+    appList.ItemsSource = apps;
+}
+
+private void launchButton_Click(object sender, RoutedEventArgs e)
+{
+    AppInfo selectedApp = (AppInfo)appList.SelectedItem;
+
+    if (selectedApp != null)
+    {
+        Process.Start(selectedApp.Path);
+    }
+}
 
